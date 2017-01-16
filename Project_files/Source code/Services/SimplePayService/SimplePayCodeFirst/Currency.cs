6a96f9a1a032b14cd58bd955c4ba5c16c1543ce7
mmarkova <markoffkaa@gmail.com>
2017-01-16 20:07:30 +0200

    db classes

---
+++ Project_files/Source code/Services/SimplePayService/SimplePayCodeFirst/Currency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePayCodeFirst
+{
+    public class Currency
+    {
+        public int ID { get; set; }
+        public string ShortName { get; set; }
+        public string FullName { get; set; }
+
+
+       
+
+    }
+}
---
+++ Project_files/Source code/Services/SimplePayService/SimplePayCodeFirst/CurrencyForDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimplePayCodeFirst
+{
+    public class CurrencyForDate
+    {
+        public int ID { get; set; }
+        public DateTime DateTime { get; set; }
+        public double Multiplier { get; set; }
+
+        public virtual Currency Currency { get; set; }
+        public int CurrencyID { get; set; }
+
+
+    }
+}
