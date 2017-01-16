1458996e2f524dfd0f20b0aaf69ec0affd15e86c
YaZhyvyi <yazhyvyi@gmail.com>
2017-01-16 21:19:02 +0200

    Updated Client_Forms_LogIn

---
+++ Project_files/Source code/Clients/SimplePay.Forms/SimplePay.From.Login/SimplePay_Client_Form_LogIn_Folder/LogIn.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(58, 29);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(188, 56);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Sign in";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(12, 105);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Username:";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(197, 223);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 26);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Enter";
+			this.button1.UseVisualStyleBackColor = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(15, 131);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(257, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(12, 160);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 22);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Password:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(15, 185);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(257, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "SimplePay: @SignIn";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+	}
+}
+
