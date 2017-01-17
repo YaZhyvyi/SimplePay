4a05509a711a56b6f07a9929248d25484e58395c
Zhekaa23 <zhekaa.zhukov@gmail.com>
2017-01-17 22:27:10 +0200

    Payment and Transfer

---
+++ Project_files/Source code/Clients/SimplePay.Forms/SimplePay.Client.Forms/UserControls/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePay_Client_Form_LogIn.UserControls
+{
+    public partial class Payment : UserControl
+    {
+        public Payment()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
