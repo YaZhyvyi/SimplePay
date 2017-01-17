39dd6407506acf2f8de6c0f86b1e904f3cfcbad5
Yura <pe.ur.ur@gmail.com>
2017-01-17 20:07:12 +0200

    add PaymentHistory UC

---
+++ Project_files/Source code/Clients/SimplePay.Forms/SimplePay.Client.Forms/UserControls/PaymentHistory.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace SimplePay_Client_Form_LogIn.UserControls
+{
+    partial class PaymentHistory
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PaymentHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "PaymentHistory";
+            this.Size = new System.Drawing.Size(555, 371);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Project_files/Source code/Clients/SimplePay.Forms/SimplePay.Client.Forms/UserControls/PaymentHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePay_Client_Form_LogIn.UserControls
+{
+    public partial class PaymentHistory : UserControl
+    {
+        public PaymentHistory()
+        {
+            InitializeComponent();
+        }
+    }
+}
