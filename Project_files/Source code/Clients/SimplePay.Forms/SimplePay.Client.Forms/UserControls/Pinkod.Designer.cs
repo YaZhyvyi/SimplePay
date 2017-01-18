01dec3cb1c1d46b104deb3aac82f181a0f780ed8
Zhekaa23 <zhekaa.zhukov@gmail.com>
2017-01-18 20:33:28 +0200

    Add Pinkod form

---
+++ Project_files/Source code/Clients/SimplePay.Forms/SimplePay.Client.Forms/UserControls/Pinkod.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace SimplePay_Client_Forms.UserControls
+{
+    partial class Pinkod
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pinkodTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your pinkod";
+            // 
+            // pinkodTextbox
+            // 
+            this.pinkodTextbox.Location = new System.Drawing.Point(113, 108);
+            this.pinkodTextbox.Name = "pinkodTextbox";
+            this.pinkodTextbox.Size = new System.Drawing.Size(100, 20);
+            this.pinkodTextbox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(14, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pinkod";
+            // 
+            // Pinkod
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pinkodTextbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Pinkod";
+            this.Size = new System.Drawing.Size(593, 409);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pinkodTextbox;
+        private System.Windows.Forms.Label label2;
+    }
+}
